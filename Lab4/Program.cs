5fac0fbebbe202313b7d5440f310e192381ff7aa
vasil <ilon.maskov.2002@mail.ru>
2023-10-29 20:52:11 +0300

    ;

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,18 +1,14 @@
 ﻿
-using System.Security.Cryptography;
-using System.Xml.XPath;
-
 var t = new Task3();
-LFSR lfsr = new LFSR(UInt16.MaxValue);
-lfsr.Generate(UInt16.MaxValue);
 
 public class Task3
 {
     readonly string _originalPath = Environment.CurrentDirectory + "/original.txt";
     readonly string _encodedPath = Environment.CurrentDirectory + "/encoded.txt";
     readonly string _decodedPath = Environment.CurrentDirectory + "/decoded.txt";
-    CongruentGenerator congruentGenerator = new CongruentGenerator(40693, UInt16.MaxValue);
-    LFSR LFSR = new LFSR(UInt16.MaxValue);
+
+    CongruentGenerator congruentGenerator = new CongruentGenerator(40693, 16);
+    LFSR lfsr = new LFSR(17);
 
     //public void Rewrite()
     //{
@@ -21,14 +17,16 @@ public class Task3
     //    File.WriteAllText(_originalPath, result);
     //}
 
-    //public void Encode()
-    //{
-    //    var text = File.ReadAllText(_originalPath);
-    //    var k = Int32.Parse(Console.ReadLine()!);
+    public void Encode()
+    {
+        var text = File.ReadAllText(_originalPath);
 
-    //    var result = Caesar.Encode(text, k);
-    //    File.WriteAllText(_encodedPath, result);
-    //}
+        int firstStage = congruentGenerator.Generate(12345);
+        string gamma = lfsr.GetGamma(firstStage, 128);
+
+
+        //File.WriteAllText(_encodedPath, result);
+    }
 
     //public void Find()
     //{
@@ -52,7 +50,6 @@ public static class Helpers
         }
         return a;
     }
-
     public static int GetMaxB(int m)
     {
         int b = m / 2 - 1;
@@ -62,7 +59,6 @@ public static class Helpers
         }
         return b;
     }
-
     public static int Pow(int number,int degree)
     {
         for (int i = 0;i<degree;i++)
@@ -71,17 +67,15 @@ public static class Helpers
         }
         return number;
     }
-
     public static string IntToBinaryString(int number, int rate)
     {
         string s = "";
         for (int i = 0; i<rate;i++)
         {
-             s += ((byte)(number >> i & 1)).ToString();
+             s += (number >> i & 1).ToString();
         }
         return s;
     }
-
     public static int XOR(string s)
     {
         int result = 0;
@@ -102,11 +96,13 @@ public class CongruentGenerator
     readonly int a ;
     readonly int b ;
     readonly int m ;
+    readonly int rate ;
 
-    public CongruentGenerator(int a, int m)
+    public CongruentGenerator(int a, int rate)
     {
         this.a = a;
-        this.m = m;
+        this.rate = rate;
+        this.m = Helpers.Pow(1,rate);
         this.b = Helpers.GetMaxB(m);
     }
 
@@ -119,18 +115,36 @@ public class CongruentGenerator
 public class LFSR
 {
     readonly int m;
-    public LFSR(int m)
+    readonly int rate;
+    public LFSR(int rate)
     {
-        this.m = m;
+        this.m = Helpers.Pow(1,rate);
+        this.rate = rate;
+    }
+
+    public string GetGamma(int start, int lenght)
+    {
+        string s = "";
+        int el = start;
+        for (int i = 0; i < (int)Math.Ceiling((double)lenght/rate); i++)
+        {
+            el = Generate(el);
+            s += Helpers.IntToBinaryString(el, rate);
+        }
+        return s[..128];
     }
 
     public int Generate(int start)                      
     {
         start = (start << 1) % m;
-        int xorTmp = Helpers.XOR(((uint)start));
+        int xorTmp = Helpers.XOR((uint)start);
         start = xorTmp ^ start;
 
+        return start;
+    }
 
+    public int Generate2(int start)
+    {
         //string bin = Helpers.IntToBinaryString(start, 16);
 
         //Console.WriteLine(Convert.ToString(start, toBase: 2));
@@ -140,12 +154,6 @@ public class LFSR
         //var n = System.Numerics.BitOperations.(start);
 
         //Console.WriteLine(Convert.ToString(n, toBase: 2));
-
-        return start;
-    }
-
-    public int Generate2(int start)
-    {
         return 1;
     }
 }
