e8f6223ca05c9cc3757f811367b1c8c27bac9225
Vasiliy Kashtanov <79720105+Vikot10@users.noreply.github.com>
2023-10-29 17:49:13 +0300

    lab 4

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,68 +1,64 @@
 ﻿
+using System.Security.Cryptography;
+using System.Xml.XPath;
+
+var t = new Task3();
+LFSR lfsr = new LFSR(UInt16.MaxValue);
+lfsr.Generate(UInt16.MaxValue);
+
 public class Task3
 {
     readonly string _originalPath = Environment.CurrentDirectory + "/original.txt";
     readonly string _encodedPath = Environment.CurrentDirectory + "/encoded.txt";
     readonly string _decodedPath = Environment.CurrentDirectory + "/decoded.txt";
-    CongruentGenerator congruentGenerator = new CongruentGenerator(40693, Helpers.Pow(2,16));
-    public void Rewrite()
-    {
-        var text = File.ReadAllText(_originalPath);
-        var result = Caesar.ProofAndRewrite(text); 
-        File.WriteAllText(_originalPath, result);
-    }
+    CongruentGenerator congruentGenerator = new CongruentGenerator(40693, UInt16.MaxValue);
+    LFSR LFSR = new LFSR(UInt16.MaxValue);
 
-    public void Encode()
-    {
-        var text = File.ReadAllText(_originalPath);
-        var k = Int32.Parse(Console.ReadLine()!);
+    //public void Rewrite()
+    //{
+    //    var text = File.ReadAllText(_originalPath);
+    //    var result = Caesar.ProofAndRewrite(text); 
+    //    File.WriteAllText(_originalPath, result);
+    //}
 
-        var result = Caesar.Encode(text, k);
-        File.WriteAllText(_encodedPath, result);
-    }
+    //public void Encode()
+    //{
+    //    var text = File.ReadAllText(_originalPath);
+    //    var k = Int32.Parse(Console.ReadLine()!);
 
-    public void Find()
-    {
-        var text = File.ReadAllText(_encodedPath);
+    //    var result = Caesar.Encode(text, k);
+    //    File.WriteAllText(_encodedPath, result);
+    //}
 
-        (var result, var key) = Caesar.FindBestKey(text);
-        Console.WriteLine($"Лучший ключ = {key}");
-        File.WriteAllText(_decodedPath, result);
-    }
+    //public void Find()
+    //{
+    //    var text = File.ReadAllText(_encodedPath);
+
+    //    (var result, var key) = Caesar.FindBestKey(text);
+    //    Console.WriteLine($"Лучший ключ = {key}");
+    //    File.WriteAllText(_decodedPath, result);
+    //}
 }
 
 public static class Helpers
 {
     static int GCD(int a, int b)
     {
-        if (a == 0)
+        while (b != 0)
         {
-            return b;
+            var t = b;
+            b = a % b;
+            a = t;
         }
-        else
-        {
-            while (b != 0)
-            {
-                if (a > b)
-                {
-                    a -= b;
-                }
-                else
-                {
-                    b -= a;
-                }
-            }
-
-            return a;
-        }
+        return a;
     }
 
     public static int GetMaxB(int m)
     {
-        int b = m - 1;
+        int b = m / 2 - 1;
         while (GCD(b, m) != 1)
         {
-            b--;
+            b++;
         }
         return b;
     }
@@ -74,6 +70,30 @@ public static class Helpers
             number *= number;
         }
         return number;
+    }
+
+    public static string IntToBinaryString(int number, int rate)
+    {
+        string s = "";
+        for (int i = 0; i<rate;i++)
+        {
+             s += ((byte)(number >> i & 1)).ToString();
+        }
+        return s;
+    }
+
+    public static int XOR(string s)
+    {
+        int result = 0;
+        foreach (var ch in s)
+        {
+            result ^= Int16.Parse(ch.ToString());
+        }
+        return result;
+    }
+    public static int XOR(UInt32 number)
+    {
+        return System.Numerics.BitOperations.PopCount(number) & 1;
     }
 }
 
@@ -94,5 +114,38 @@ public class CongruentGenerator
     {
         return (a * start + b) % m;
     }
+}
 
+public class LFSR
+{
+    readonly int m;
+    public LFSR(int m)
+    {
+        this.m = m;
+    }
+
+    public int Generate(int start)                      
+    {
+        start = (start << 1) % m;
+        int xorTmp = Helpers.XOR(((uint)start));
+        start = xorTmp ^ start;
+
+
+        //string bin = Helpers.IntToBinaryString(start, 16);
+
+        //Console.WriteLine(Convert.ToString(start, toBase: 2));
+        //start <<= 1;
+        //Console.WriteLine(Convert.ToString(start, toBase: 2));
+
+        //var n = System.Numerics.BitOperations.(start);
+
+        //Console.WriteLine(Convert.ToString(n, toBase: 2));
+
+        return start;
+    }
+
+    public int Generate2(int start)
+    {
+        return 1;
+    }
 }
